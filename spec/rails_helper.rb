b83b40929cd4d86c9814efab948bacd02fe2e95d
Nico Rithner <nicorithner@gmail.com>
2020-10-25 18:52:51 -0600

    Update VCR configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,6 +74,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data('WEATHER_API_KEY') {ENV['WEATHER_API_KEY']}
   config.filter_sensitive_data('MAP_API_KEY') {ENV['MAP_API_KEY']}
+  config.filter_sensitive_data('IMAGE_API_KEY') {ENV['IMAGE_API_KEY']}
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
