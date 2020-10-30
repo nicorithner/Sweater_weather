a4c75e713087495bde82e8661c3fd01bc600420a
Nico Rithner <nicorithner@gmail.com>
2020-10-29 21:56:06 -0600

    Update weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,9 +1,9 @@
 class WeatherService
-  def  self.get_forecast(coordinates)
+  def  self.get_forecast(location)
     response = conn.get('data/2.5/onecall?') do |f|
       f.params[:appid] = ENV['WEATHER_API_KEY']
-      f.params[:lat] = coordinates[:lat]
-      f.params[:lon] = coordinates[:lng]
+      f.params[:lat] = location.latitude
+      f.params[:lon] = location.longitude
       f.params[:units] = 'imperial'
       f.params[:exclude] = 'minutely'
     end
