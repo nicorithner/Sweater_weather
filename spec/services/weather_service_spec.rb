a1e8d303b6d2cb6356b51922c7758a654a79e37a
Nico Rithner <nicorithner@gmail.com>
2020-10-29 21:54:53 -0600

    Update weather service spec

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe WeatherService do
   it 'can get the forecast from lat/long coordinates' do
     VCR.use_cassette('forecast_search') do
-      coordinates = { lat: 39.738453, lng: -104.984853 }
-      forecast = WeatherService.get_forecast(coordinates)
+      coordinates = MapService.get_coordinates('denver, co')
+      location = Location.new(coordinates)
+      forecast = WeatherService.get_forecast(location)
 
       expect(forecast).to be_a(Hash)
       expect(forecast[:current]).to have_key(:sunrise)
