8255f65ade12b67bbc6a86f4ae1754bd917f18ae
Nico Rithner <nicorithner@gmail.com>
2020-10-30 19:42:10 -0600

    Update spec_helper simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,14 @@
 require 'webmock/rspec'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails'
+SimpleCov.start do
+  add_filter 'app/channels'
+  add_filter 'app/jobs'
+  add_filter 'app/mailers'
+  add_filter 'app/models/application_record.rb'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
