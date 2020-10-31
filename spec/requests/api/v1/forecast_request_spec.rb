6a4c75fb9dc216d6605425b28d51e5c483381817
Nico Rithner <nicorithner@gmail.com>
2020-10-30 18:33:53 -0600

    Updated test to match name assigments

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,32 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Forecasts', type: :request do
-  describe 'Forecast API', :vcr do
+  describe 'Forecast API' do
     it 'Return specific city-state forecast data' do
-      
-      get '/api/v1/forecast?location=denver,co'
-      json = JSON.parse(response.body, symbolize_names: true)
+      VCR.use_cassette('forecast_search') do
+        get '/api/v1/forecast?location=denver, co'
+        json = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_successful
-      expect(json[:data]).to have_key([:id, :type, :attributes])
-      expect(json[:data][:attributes]).to have_key([:current_weather, :daily_weather, :hourly_weather])
-      expect(json[:data][:attributes]).to not_have_key([:minutely_weather])
+        expect(response).to be_successful
+        expect(json[:data]).to have_key(:id)
+        expect(json[:data]).to have_key(:type)
+        expect(json[:data]).to have_key(:attributes)
+        expect(json[:data][:attributes]).to have_key(:current_weather)
+        expect(json[:data][:attributes]).to have_key(:daily_weather)
+        expect(json[:data][:attributes]).to have_key(:hourly_weather)
+        expect(json[:data][:attributes]).to_not have_key(:minutely_weather)
 
-      expect(json[:data][:type]).to eq('forecast')
-      expect(json[:data][:attributes][:timezone]).to be_a(String)
-      expect(json[:data][:attributes][:current][:sunrise]).to be_a(Integer)
-      expect(json[:data][:attributes][:current][:sunset]).to be_a(Integer)
-      expect(json[:data][:attributes][:current][:temperature]).to be_a(Float)
-      expect(json[:data][:attributes][:current][:weather][0][:description]).to be_a(String)
-      expect(json[:data][:attributes][:hourly][0][:wind_speed]).to be_a(String)
-      expect(json[:data][:attributes][:place]).to eq('Denver, CO')
-      
-      expect(json[:data][:attributes][:daily][0][:feels_like]).to be_nil
-      expect(json[:data][:attributes][:daily][0][:visibility]).to be_nil
-      expect(json[:data][:attributes][:hourly][0][:visibility]).to be_nil
+        expect(json[:data][:type]).to eq('forecast')
+        expect(json[:data][:attributes][:timezone]).to be_a(String)
+        expect(json[:data][:attributes][:current_weather][:sunrise]).to be_a(Integer)
+        expect(json[:data][:attributes][:current_weather][:sunset]).to be_a(Integer)
+        expect(json[:data][:attributes][:current_weather][:temp]).to be_a(Float)
+        expect(json[:data][:attributes][:current_weather][:weather][0][:description]).to be_a(String)
+        expect(json[:data][:attributes][:hourly_weather][0][:wind_speed]).to be_a(Float)
+      end
     end
   end
 end
+
+# json[:current].keys
+# => [:dt, :sunrise, :sunset, :temp, :feels_like, :pressure, :humidity, :dew_point, :uvi, :clouds, :visibility, :wind_speed, :wind_deg, :weather]
+
+## in the controller index getting this 
+# forecast.keys
+# => [:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily]
 
 =begin 
 
