d14f4e84652ec190b75d4f0d7b7e16f9c9c01d53
Nico Rithner <nicorithner@gmail.com>
2020-10-30 19:42:59 -0600

    Upgrade forecast controller to use facade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    coordinates = MapService.get_coordinates(params[:location])
-    location = Location.new(coordinates)
-    forecast = Forecast.new(WeatherService.get_forecast(location))
-    json = render json: ForecastSerializer.new(forecast)
+    forecast = ForecastFacade.get_forecast(params[:location])
+    render json: ForecastSerializer.new(forecast)
   end
 end
