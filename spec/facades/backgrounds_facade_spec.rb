b5ec1b1a27b69ae27ff21ca918a77d71b08e5004
Nico Rithner <nicorithner@gmail.com>
2020-11-05 13:03:19 -0700

    Background facade spec

---
+++ spec/facades/backgrounds_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Backgrounds Facade' do
+  it 'Expose a json with image attributes' do
+    VCR.use_cassette('background') do
+      location = 'denver,co'
+      background_image = BackgroundsFacade.get_image(location)
+
+      expect(background_image).to be_a(Image)
+      expect(background_image.id).to eq(nil) 
+      expect(background_image.location).to be_a(String) 
+      expect(background_image.image_url).to be_a(String) 
+      expect(background_image.source).to be_a(String) 
+      expect(background_image.author).to be_a(String) 
+      expect(background_image.logo).to be_a(String)
+    end
+  end
+end
