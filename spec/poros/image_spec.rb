5c364ec2f114ebdad76ba5f15a135f2bc9f6ce90
Nico Rithner <nicorithner@gmail.com>
2020-11-05 10:25:00 -0700

    Update spec
    Removed the spec expectation
    to have an image and a credit hash in the poro
    Will do that in the serializer

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 describe Image do
   it 'has attributes' do
-    
-    data = ImageService.get_image('denver, co')
-    background_image = Image.new(data)
+    VCR.use_cassette('background') do
+      data = ImageService.get_image('denver, co')
+      background_image = Image.new(data)
 
-    expect(background_image).to be_a(Image)
-    expect(background_image.id).to eq('nil') 
-    expect(background_image.type).to be_a(String) 
-    expect(background_image.image).to be_a(Hash) 
-    expect(background_image.image[:location]).to be_a(String) 
-    expect(background_image.image[:image_url]).to be_a(String) 
-    expect(background_image.image[:credit]).to be_a(Hash) 
-    expect(background_image.image[:credit][:source]).to be_a(String) 
-    expect(background_image.image[:credit][:author]).to be_a(String) 
-    expect(background_image.image[:credit][:logo]).to be_a(String) 
+      expect(background_image).to be_a(Image)
+      expect(background_image.id).to eq(nil) 
+      expect(background_image.type).to be_a(String) 
+      expect(background_image.location).to be_a(String) 
+      expect(background_image.image_url).to be_a(String) 
+      expect(background_image.source).to be_a(String) 
+      expect(background_image.author).to be_a(String) 
+      expect(background_image.logo).to be_a(String)
+    end 
   end
 end
