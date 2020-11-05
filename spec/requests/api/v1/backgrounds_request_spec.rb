e02087d1571cae62549573e0733aa74f2f9120de
Nico Rithner <nicorithner@gmail.com>
2020-11-05 13:02:55 -0700

    Create background request spec

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe 'Api::V1::Backgrounds' do
       VCR.use_cassette('background') do
         get '/api/v1/backgrounds?location=denver,co'
         json = JSON.parse(response.body, symbolize_names: :true)
-        
+        binding.pry
         expect(response).to be_successful
         expect(json[:data]).to have_key(:id)
         expect(json[:data]).to have_key(:type)
         expect(json[:data]).to have_key(:image)
-        expect(json[:data][:image]).to have_key(:location)
-        expect(json[:data][:image]).to have_key(:image_url)
-        expect(json[:data][:image]).to have_key(:credit)
-        expect(json[:data][:image][:credit]).to have_key(:source)
-        expect(json[:data][:image][:credit]).to have_key(:author)
-        expect(json[:data][:image][:credit]).to have_key(:logo)
+        expect(json[:data][:attributes][:image]).to have_key(:location)
+        expect(json[:data][:attributes][:image]).to have_key(:image_url)
+        expect(json[:data][:attributes][:image]).to have_key(:credit)
+        expect(json[:data][:attributes][:image][:credit]).to have_key(:source)
+        expect(json[:data][:attributes][:image][:credit]).to have_key(:author)
+        expect(json[:data][:attributes][:image][:credit]).to have_key(:logo)
       end
     end
   end
