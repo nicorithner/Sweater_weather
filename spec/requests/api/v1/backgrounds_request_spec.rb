60cff178d06b55e7f1b8ebf92fd912e5ea77f5c9
Nico Rithner <nicorithner@gmail.com>
2020-11-05 10:45:46 -0700

    Create backgrounds request spec

---
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Backgrounds' do
+  describe 'Background API' do
+    it "Background request returns a json with image attributes" do
+      VCR.use_cassette('background') do
+        get '/api/v1/backgrounds?location=denver,co'
+        json = JSON.parse(response.body, symbolize_names: :true)
+        
+        expect(response).to be_successful
+        expect(json[:data]).to have_key(:id)
+        expect(json[:data]).to have_key(:type)
+        expect(json[:data]).to have_key(:image)
+        expect(json[:data][:image]).to have_key(:location)
+        expect(json[:data][:image]).to have_key(:image_url)
+        expect(json[:data][:image]).to have_key(:credit)
+        expect(json[:data][:image][:credit]).to have_key(:source)
+        expect(json[:data][:image][:credit]).to have_key(:author)
+        expect(json[:data][:image][:credit]).to have_key(:logo)
+      end
+    end
+  end
+end
