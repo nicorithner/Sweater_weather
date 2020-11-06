35780ca43cddcd62bdac7f8fa5942e01fc2baf48
Nico Rithner <nicorithner@gmail.com>
2020-11-06 07:30:28 -0700

    Add login test to users_request_spec

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -4,19 +4,40 @@ RSpec.describe 'Users API' do
   describe 'User registration api' do
     it 'A successful request creates a user in your database, and generates a unique api key associated with that user, with a 201 status code' do
       user_params = ({
-                      email: "whatever@example.com",
-                      password: "password",
-                      password_confirmation: "password"
+                      email: 'whatever@example.com',
+                      password: 'password',
+                      password_confirmation: 'password'
                     })
-      headers = {"CONTENT_TYPE" => "application/json"}
+      headers = {'CONTENT_TYPE' => 'application/json'}
 
-      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
       json = JSON.parse(response.body, symbolize_names: true)
 
       user = User.last
 
       expect(response).to be_successful
 
+      expect(user.email).to eq(json[:data][:attributes][:email])
+      expect(user.api_key).to eq(json[:data][:attributes][:api_key])
+      expect(json[:data][:attributes]).not_to have_key([:password])
+      expect(json[:data][:id]).not_to eq(nil)
+      expect(json[:data][:type]).to eq('users')
+    end
+  end
+
+  describe 'Users Login Api' do
+    it "A successful request returns the user’s api key" do
+      login_params =
+                    {
+                      email: 'whatever@example.com',
+                      password: 'password'
+                    }
+      headers = {'CONTENT_TYPE' => 'application/json'}
+  
+      post 'api/v1/sessions', headers: headers, params: JSON.generate(login_params)
+      json = JSON.parse(response.body, symbolize_names: :true)
+  
+      expect(response).to be_successful
       expect(user.email).to eq(json[:data][:attributes][:email])
       expect(user.api_key).to eq(json[:data][:attributes][:api_key])
       expect(json[:data][:attributes]).not_to have_key([:password])
