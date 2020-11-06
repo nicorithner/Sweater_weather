c6f39b8cbaa1be27920d888e18cd721f35bf1101
Nico Rithner <nicorithner@gmail.com>
2020-11-05 18:52:11 -0700

    Create user model validation spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe "Validation" do
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+    it { should validate_presence_of :password}
+    it { should validate_presence_of :password_confirmation}
+  end
+end
