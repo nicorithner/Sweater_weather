78975304703e5bde9f16ef5d426601143f2879c6
Nico Rithner <nicorithner@gmail.com>
2020-11-05 19:21:16 -0700

    Add creation of user object with api_key
    Added also 'dreamed code' for the serializer
    Serializer hasn't been created yet

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    user = User.new(user_params)
+    user.save
+    user.api_key
+    # binding.pry
+    render json: UsersSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
