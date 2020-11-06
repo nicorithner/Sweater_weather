cd687f35b6eed680be8de7a801760b2551e7904f
Nico Rithner <nicorithner@gmail.com>
2020-11-05 19:42:12 -0700

    Update test
    Added json parse and more expectations
    Needs to be upgraded. Just happy path for now

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -9,15 +9,19 @@ RSpec.describe 'Users API' do
                       password_confirmation: "password"
                     })
       headers = {"CONTENT_TYPE" => "application/json"}
+
       post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
-      
+      json = JSON.parse(response.body, symbolize_names: true)
+
       user = User.last
 
       expect(response).to be_successful
 
-      expect(user.email).to eq(response[:data][:attributes][:email])
-      expect(user.api_key).to eq(response[:data][:attributes][:api_key])
-      expect(response[:data][:attributes]).not_to have_key([:password])
+      expect(user.email).to eq(json[:data][:attributes][:email])
+      expect(user.api_key).to eq(json[:data][:attributes][:api_key])
+      expect(json[:data][:attributes]).not_to have_key([:password])
+      expect(json[:data][:id]).not_to eq(nil)
+      expect(json[:data][:type]).to eq('users')
     end
   end
 end
