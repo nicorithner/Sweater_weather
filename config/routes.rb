beef532b693483453fb8e8f0d278a5048f810239
Nico Rithner <nicorithner@gmail.com>
2020-11-06 10:07:34 -0700

    Add sessions route 'create'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
-      resource :users, only: [:create]
+      resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
