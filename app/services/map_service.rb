45f6dc65332e8acc808bc71d598867da9799695b
Nico Rithner <nicorithner@gmail.com>
2020-11-07 16:06:58 -0700

    Create get_route mapservice method

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -12,4 +12,21 @@ class MapService
   def self.conn
     Faraday.new('https://www.mapquestapi.com/')
   end
+
+  def self.get_route(origin, destination)
+    response = conn.get('directions/v2/route') do |f|
+      f.params[:key] = ENV['MAP_API_KEY']
+      f.params[:from] = origin
+      f.params[:to] = destination
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://www.mapquestapi.com/')
+  end
+
+
 end
+#http://www.mapquestapi.com/directions/v2/route?key=lEqLhOepdVCpmyz6y6eMQZt8jtRBUO0b&from=New York, NY&to=Los Angeles, CA
