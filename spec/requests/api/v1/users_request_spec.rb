626b0fc88b4d0cc1d79381def010bb0c0eb6d097
Nico Rithner <nicorithner@gmail.com>
2020-11-06 17:50:51 -0700

    Add user registration sad path testing

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -23,6 +23,42 @@ RSpec.describe 'Users API' do
       expect(json[:data][:id]).not_to eq(nil)
       expect(json[:data][:type]).to eq('users')
     end
+
+    describe "Test for sad path. An unsuccessful request returns corresponding 400 status body with error description" do
+      it "Blank email request returns status 406 and a body with error description" do
+        user_params = ({
+                        password: 'password123',
+                        password_confirmation: 'password123'
+                      })
+        headers = {'CONTENT_TYPE' => 'application/json'}
+  
+        post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+        json = JSON.parse(response.body, symbolize_names: :true)
+
+        user = User.last
+  
+        expect(response).not_to be_successful
+        expect(response).to have_http_status(406)
+        expect(json[:message]).to eq('Email cannot be blank')
+      end
+
+      it "Blank password request returns status 406 and a body with error description" do
+        user_params = ({
+                        email: 'another@example.com',
+                        password_confirmation: 'password123'
+                      })
+        headers = {'CONTENT_TYPE' => 'application/json'}
+  
+        post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+        json = JSON.parse(response.body, symbolize_names: :true)
+        
+        user = User.last
+  
+        expect(response).not_to be_successful
+        expect(response).to have_http_status(406)
+        expect(json[:message]).to eq('Password cannot be blank')
+      end
+    end
   end
 
   describe 'Users Login Api' do
