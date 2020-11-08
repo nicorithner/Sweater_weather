78720ea505742b461118c2a9dbc3f7022c19746f
Nico Rithner <nicorithner@gmail.com>
2020-11-07 19:05:59 -0700

    Add sad path to controller plus 'dream code'
    I would like to do everything with a facade but
    first I will run each step in the controller
    so it is easier to test then I refactor to the facade

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
+    if user
+      # trip_data = RoadTripFacade.get_trip_data(params[:origin], params[:destination])
+      
+      json = render json: RoadTripSerializer.new(trip_data), status: :ok
+      binding.pry
+    elsif params[:origin].nil?
+      render json: { message: 'Origin cannot be blank' }, status: :not_acceptable
+    elsif params[:destination].nil?
+      render json: { message: 'Destination cannot be blank' }, status: :not_acceptable
+    else
+      render json: { message: 'Incorrect credentials' }, status: :unauthorized
+    end
     
-    binding.pry
   end
 end
