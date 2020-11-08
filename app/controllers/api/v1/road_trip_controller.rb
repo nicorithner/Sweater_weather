395d5f27fda612001c016719d29f93cdebbcc233
Nico Rithner <nicorithner@gmail.com>
2020-11-08 13:02:58 -0700

    Add methods to return json to roadtrip request
    Needs refactor. These will move to a facade

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,14 +3,13 @@ class Api::V1::RoadTripController < ApplicationController
     user = User.find_by(api_key: params[:api_key])
     if user
       # trip_data = RoadTripFacade.get_trip_data(params[:origin], params[:destination])
-      
-      # trip = Mapservice to get travel_time
-      # destination = Mapservice to get destination lat and long
-      # ETA = Add travel time to time.now to calculate ETA
-      # ETA_weather = Weatherservice to get weather at ETA
-      # create object. user.roadtrips.create(origin: trip[:origin], destination: trip[:destination] , duration: trip[:duration], temperature: ETA_weather[:temperature] , description: ETA_weather[:description] )
-      json = render json: RoadTripSerializer.new(trip_data), status: :ok
-      binding.pry
+      trip = MapService.get_route(params[:origin], params[:destination])
+      dest_coords = MapService.get_coordinates(params[:destination])
+      days = (trip[:route][:time].to_f / 60 / 60 / 24).round(2)
+      eta = (DateTime.now + days).beginning_of_hour.to_i # => Tue, 10 Nov 2020 02:00:00 -0700
+      eta_forecast = WeatherService.get_eta_forecast(dest_coords, eta)
+      road_trip = RoadTrip.create(origin: params[:origin], destination: params[:destination] , duration: trip[:route][:formattedTime], temperature: eta_forecast[:temp] , description: eta_forecast[:weather][0][:description], user_id: user.id )
+      render json: RoadTripSerializer.new(road_trip)
     elsif params[:origin].nil?
       render json: { message: 'Origin cannot be blank' }, status: :not_acceptable
     elsif params[:destination].nil?
