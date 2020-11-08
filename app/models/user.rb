f4926a14cc27b23663786fbc7ec2f74aca3a4e22
Nico Rithner <nicorithner@gmail.com>
2020-11-07 19:24:33 -0700

    Add user has_many :roadtrips

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
-
+  has_many :roadtrips, dependent: :destroy
   validates :email, :password_confirmation, presence: true
   validates_uniqueness_of :email
 end
