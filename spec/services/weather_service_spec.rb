c3b44d6eee8e7b2d1b7b88a2a618d2b8a114ba07
Nico Rithner <nicorithner@gmail.com>
2020-11-08 12:58:03 -0700

    Update weather_service_spec

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -16,17 +16,19 @@ RSpec.describe WeatherService do
   end
   it 'can get the eta forecast from lat/long coordinates' do
     VCR.use_cassette('eta_forecast_search') do
-      coordinates = MapService.get_coordinates('denver, co')
-      location = Location.new(coordinates)
-      eta_forecast = WeatherService.get_eta_forecast(location)
-
+      trip = MapService.get_route('New York, NY', 'Los Angeles, CA')
+      dest_coords = MapService.get_coordinates('Los Angeles, CA')
+      days = (trip[:route][:time].to_f / 60 / 60 / 24).round(2)
+      eta = (DateTime.now + days).beginning_of_hour.to_i
+      eta_forecast = WeatherService.get_eta_forecast(dest_coords, eta)
+      
       expect(eta_forecast).to be_a(Hash)
       expect(eta_forecast).to have_key(:dt)
       expect(eta_forecast).to have_key(:temp)
-      expect(eta_forecast).to have_key(:description)
+      expect(eta_forecast[:weather][0]).to have_key(:description)
       expect(eta_forecast[:dt]).to be_a(Numeric)
       expect(eta_forecast[:temp]).to be_a(Numeric)
-      expect(eta_forecast[:description]).to be_a(String)
+      expect(eta_forecast[:weather][0][:description]).to be_a(String)
     end
   end
 end
