089aedf601059a58bde5cc91081a1b0dba5912fd
Nico Rithner <nicorithner@gmail.com>
2020-11-07 19:56:52 -0700

    Add commented out dream code to drive development

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,6 +4,11 @@ class Api::V1::RoadTripController < ApplicationController
     if user
       # trip_data = RoadTripFacade.get_trip_data(params[:origin], params[:destination])
       
+      # trip = Mapservice to get travel_time
+      # destination = Mapservice to get destination lat and long
+      # ETA = Add travel time to time.now to calculate ETA
+      # ETA_weather = Weatherservice to get weather at ETA
+      # create object. user.roadtrips.create(origin: trip[:origin], destination: trip[:destination] , duration: trip[:duration], temperature: ETA_weather[:temperature] , description: ETA_weather[:description] )
       json = render json: RoadTripSerializer.new(trip_data), status: :ok
       binding.pry
     elsif params[:origin].nil?
