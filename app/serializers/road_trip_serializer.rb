e03589983ba60d998a00a5745a0c2f6dcbcdee20
Nico Rithner <nicorithner@gmail.com>
2020-11-08 12:59:47 -0700

    Create roadtrip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,4 +1,20 @@
 class RoadTripSerializer
   include JSONAPI::Serializer
-  attributes :id, :origin, :destination, :duration, :temperature, :description
+  set_id{nil}
+
+  attribute :start_city do |object|
+    object.origin
+  end
+
+  attribute :end_city do |object|
+    object.destination
+  end
+
+  attribute :travel_time do |object|
+    object.duration
+  end
+
+  attribute :weather_at_eta do |object|
+    {temperature: object.temperature, conditions: object.description}
+  end
 end
