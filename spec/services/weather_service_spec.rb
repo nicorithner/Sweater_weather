063113133cacfbc6de0da546b2e6bb1f4d014ca3
Nico Rithner <nicorithner@gmail.com>
2020-11-08 12:02:29 -0700

    Add eta_forecast test to weather_service spec

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe WeatherService do
       expect(forecast[:current]).to have_key(:pressure)
     end
   end
+  it 'can get the eta forecast from lat/long coordinates' do
+    VCR.use_cassette('eta_forecast_search') do
+      coordinates = MapService.get_coordinates('denver, co')
+      location = Location.new(coordinates)
+      eta_forecast = WeatherService.get_eta_forecast(location)
+
+      expect(eta_forecast).to be_a(Hash)
+      expect(eta_forecast).to have_key(:dt)
+      expect(eta_forecast).to have_key(:temp)
+      expect(eta_forecast).to have_key(:description)
+      expect(eta_forecast[:dt]).to be_a(Numeric)
+      expect(eta_forecast[:temp]).to be_a(Numeric)
+      expect(eta_forecast[:description]).to be_a(String)
+    end
+  end
 end
