cfaba229575be2a2d71867c87a547d817f6784df
Nico Rithner <nicorithner@gmail.com>
2020-11-09 15:51:34 -0700

    Test json have key 'data' and daily weather count is 5

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe 'Api::V1::Forecasts', type: :request do
         json = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
+        expect(json).to have_key(:data)
         expect(json[:data]).to have_key(:id)
         expect(json[:data]).to have_key(:type)
         expect(json[:data]).to have_key(:attributes)
         expect(json[:data][:attributes]).to have_key(:current_weather)
         expect(json[:data][:attributes]).to have_key(:daily_weather)
+        expect(json[:data][:attributes][:daily_weather].count).to eq(5)
         expect(json[:data][:attributes]).to have_key(:hourly_weather)
         expect(json[:data][:attributes]).to_not have_key(:minutely_weather)
 
@@ -27,49 +29,3 @@ RSpec.describe 'Api::V1::Forecasts', type: :request do
     end
   end
 end
-
-# json[:current].keys
-# => [:dt, :sunrise, :sunset, :temp, :feels_like, :pressure, :humidity, :dew_point, :uvi, :clouds, :visibility, :wind_speed, :wind_deg, :weather]
-
-## in the controller index getting this 
-# forecast.keys
-# => [:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily]
-
-=begin 
-
-Response:
-
-The response data should contain exactly these elements and nothing more:
-
-a data attribute, under which all other attributes are present:
-  id, always set to null
-  type, always set to “forecast”
-  attributes, an object containing weather information:
-    current_weather, holds current weather data:
-      datetime, in a human-readable format such as “2020-09-30 13:27:03 -0600”
-      sunrise, in a human-readable format such as “2020-09-30 06:27:03 -0600”
-      sunset, in a human-readable format such as “2020-09-30 18:27:03 -0600”
-      temperature, floating point number indicating the current temperature in Fahrenheit
-      feels_like, floating point number indicating a temperature in Fahrenheit
-      humidity, numeric (int or float), as given by OpenWeather
-      uvi, numeric (int or float), as given by OpenWeather
-      visibility, numeric (int or float), as given by OpenWeather
-      conditions, the first ‘description’ field from the weather data as given by OpenWeather
-      icon, string, as given by OpenWeather
-    daily_weather, array of the next 5 days of daily weather data:
-      date, in a human-readable format such as “2020-09-30”
-      sunrise, in a human-readable format such as “2020-09-30 06:27:03 -0600”
-      sunset, in a human-readable format such as “2020-09-30 18:27:03 -0600”
-      max_temp, floating point number indicating the maximum expected temperature in Fahrenheit
-      min_temp, floating point number indicating the minimum expected temperature in Fahrenheit
-      conditions, the first ‘description’ field from the weather data as given by OpenWeather
-      icon, string, as given by OpenWeather
-    hourly_weather, array of the next 8 hours of hourly weather data:
-      time, in a human-readable format such as “14:00:00”
-      wind_speed, string, in miles per hour
-      wind_direction, string, check wikipedia for how to convert this numeric value
-      conditions, the first ‘description’ field from the weather data as given by OpenWeather
-      icon, string, as given by OpenWeather
-
-
-=end
