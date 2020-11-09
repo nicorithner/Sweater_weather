91857d93f016e9fc0946e81617d2650e0a92de79
Nico Rithner <nicorithner@gmail.com>
2020-11-09 15:48:20 -0700

    Remove redundant code

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,8 +1,5 @@
 class ForecastSerializer
   include JSONAPI::Serializer
-  attributes :id, :type, :current_weather, :daily_weather, :hourly_weather, :timezone
-
-  attribute :attributes do |object|
-    { current_weather: object.current_weather, daily_weather: object.daily_weather, hourly_weather: object.hourly_weather, timezone: object.timezone }
-  end
+  set_id { nil }
+  attributes :type, :current_weather, :daily_weather, :hourly_weather, :timezone
 end
