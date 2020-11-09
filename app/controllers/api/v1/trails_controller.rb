4eafda635c0867ce3fc71881f9548715a8063551
Nico Rithner <nicorithner@gmail.com>
2020-11-09 09:33:48 -0700

    Add create poro to index

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::TrailsController < ApplicationController
   def index
     coordinates = MapService.get_coordinates(params[:location])
     trails = TrailsService.new.get_trails(coordinates)
-    # binding.pry
+    location = Location.new(coordinates)
+    location_forecast = Forecast.new(WeatherService.get_forecast(location))
+    current_forecast = location_forecast.current_weather
+
+    trails_search = Trail.new(trails, current_forecast)
+    render json: TrailsSerializer.new(trails_search)
   end
 end
 
@@ -12,4 +17,8 @@ index params
   => <ActionController::Parameters {"location"=>"denver,co", "controller"=>"api/v1/trails", "action"=>"index"} permitted: false>
 
 trails call: https://www.hikingproject.com/data/get-trails?lat=39.738453&lon=-104.984853&maxDistance=10&key=200545096-5f955e77feb9332b7dc97751e8ac525e
+
+current_forecast[:temp] => 34.34
+current_forecast[:weather][0][:description] => "broken clouds"
+
 =end
