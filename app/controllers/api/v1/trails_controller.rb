c0982da5d8e9dff3d8004e4bc5f77d0bed30e033
Nico Rithner <nicorithner@gmail.com>
2020-11-09 08:38:49 -0700

    Add wished trailservice to index to get trails

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::TrailsController < ApplicationController
   def index
     coordinates = MapService.get_coordinates(params[:location])
-    binding.pry
+    trails = TrailService.new.get_trails(coordinates)
+    # binding.pry
   end
 end
 
@@ -9,4 +10,6 @@ end
 Notes:
 index params
   => <ActionController::Parameters {"location"=>"denver,co", "controller"=>"api/v1/trails", "action"=>"index"} permitted: false>
+
+trails call: https://www.hikingproject.com/data/get-trails?lat=39.738453&lon=-104.984853&maxDistance=10&key=200545096-5f955e77feb9332b7dc97751e8ac525e
 =end
