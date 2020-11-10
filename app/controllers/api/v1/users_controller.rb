4a363de2857b263538cdf76ed269ca80367bd5c2
Nico Rithner <nicorithner@gmail.com>
2020-11-10 12:21:30 -0700

    Refactor Create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    verify(user, params)
+    save_user(user, params)
   end
 
   private
 
-  def verify(user, user_params)
+  def save_user(user, user_params)
+    unless screen_fields(user_params)
+      user.save
+      user.api_key
+      render json: UsersSerializer.new(user), status: :created
+    end
+  end
+  
+  def screen_fields(user_params)
     if User.find_by(email: user_params[:email])
       render json: { message: 'Email already exists' }, status: :not_acceptable
     elsif user_params[:email].nil?
@@ -15,9 +23,6 @@ class Api::V1::UsersController < ApplicationController
       render json: { message: 'Password cannot be blank' }, status: :not_acceptable
     elsif user_params[:password_confirmation] != user_params[:password]
       render json: { message: "Passwords don't match" }, status: :not_acceptable
-    else  user.save
-          user.api_key
-          render json: UsersSerializer.new(user), status: :created
     end
   end
 
