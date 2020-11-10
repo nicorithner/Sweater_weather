d3fcf19b2ec0a4f87d09c9231a36e476cf5c564f
Nico Rithner <nicorithner@gmail.com>
2020-11-10 11:57:16 -0700

    Add not matching passwords sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,25 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    if user.save
-      user.api_key
-      render json: UsersSerializer.new(user), status: :created
-    elsif user_params[:email].nil?
-      render json: { message: 'Email cannot be blank' }, status: :not_acceptable
-    else
-      user_params[:password].nil?
-      render json: { message: 'Password cannot be blank' }, status: :not_acceptable
-    end
+    verify(user, params)
   end
 
   private
+
+  def verify(user, user_params)
+    if User.find_by(email: user_params[:email])
+      render json: { message: 'Email already exists' }, status: :not_acceptable
+    elsif user_params[:email].nil?
+      render json: { message: 'Email cannot be blank' }, status: :not_acceptable
+    elsif user_params[:password].nil?
+      render json: { message: 'Password cannot be blank' }, status: :not_acceptable
+    elsif user_params[:password_confirmation] != user_params[:password]
+      render json: { message: "Passwords don't match" }, status: :not_acceptable
+    else  user.save
+          user.api_key
+          render json: UsersSerializer.new(user), status: :created
+    end
+  end
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
