137339a471d48309a963fae39cf7d9d115cb994d
Nico Rithner <nicorithner@gmail.com>
2020-11-10 07:57:38 -0700

    Add test for content that shouldn't be present

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -59,5 +59,56 @@ RSpec.describe 'Api::V1::Forecasts', type: :request do
         expect(json[:data][:attributes][:hourly_weather].first[:icon]).to be_a(String)
       end
     end
+    it "Test it doesn't return certain data" do
+      VCR.use_cassette('forecast_search') do
+        get '/api/v1/forecast?location=denver, co'
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        ##==== Shouldn't Have Attributes
+        expect(json[:data][:attributes]).to_not have_key(:minutely_weather)
+        
+        ##==== Shouldn't Have Attributes Current Weather
+        expect(json[:data][:attributes][:current_weather].keys.count).to_not eq(11)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:pressure)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:dew_point)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:clouds)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:wind_speed)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:wind_deg)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:rain)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:id)
+        expect(json[:data][:attributes][:current_weather]).to_not have_key(:main)
+        
+        ##==== Shouldn't Have Attributes Daily Weather
+        expect(json[:data][:attributes][:daily_weather].first.keys.count).to_not eq(8)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:temp)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:feels_like)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:pressure)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:humidity)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:dew_point)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:wind_speed)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:wind_deg)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:clouds)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:pop)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:uvi)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:id)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:main)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:day)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:night)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:eve)
+        expect(json[:data][:attributes][:daily_weather].first).to_not have_key(:morn)
+
+        ##==== Shouldn't Have Attributes Hourly Weather
+        expect(json[:data][:attributes][:hourly_weather].first.keys.count).to_not eq(7)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:feels_like)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:pressure)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:humidity)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:dew_point)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:clouds)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:visibility)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:pop)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:id)
+        expect(json[:data][:attributes][:hourly_weather].first).to_not have_key(:main)
+      end
+    end
   end
 end
