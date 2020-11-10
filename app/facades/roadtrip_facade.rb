9030ec26ad42be81ce7254fc43d67c774dd3c550
Nico Rithner <nicorithner@gmail.com>
2020-11-10 11:49:08 -0700

    Refactor roadtrip facade

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -1,11 +1,15 @@
 class RoadtripFacade
   def self.get_trip_data(params)
     trip = MapService.get_route(params[:origin], params[:destination])
+    eta_forecast = find_eta_forecast(params, trip)
+    create_trip(params, trip, eta_forecast)
+  end
+
+  def self.find_eta_forecast(params, trip)
     dest_coords = MapService.get_coordinates(params[:destination])
     days = (trip[:route][:time].to_f / 60 / 60 / 24).round(2)
     eta = (DateTime.now + days).beginning_of_hour.to_i
-    eta_forecast = WeatherService.get_eta_forecast(dest_coords, eta)
-    create_trip(params, trip, eta_forecast)
+    WeatherService.get_eta_forecast(dest_coords, eta)
   end
 
   def self.create_trip(params, trip, eta_forecast)
