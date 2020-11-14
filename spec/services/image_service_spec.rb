adfe73e88a92ff7cae88d5077d2960b220e10e68
Nico Rithner <nicorithner@gmail.com>
2020-11-13 17:39:57 -0700

    Change test for refactor image service

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -4,20 +4,18 @@ RSpec.describe ImageService do
   it "Can get a city background image from a query" do
     VCR.use_cassette('background') do
       location = 'denver, co'
-      image = ImageService.get_image(location)
+      data = ImageService.get_image(location)
 
-      expect(image).to be_an(Hash)
-      expect(image[:json]).to be_a(Hash)
-      expect(image[:json][:id]).to be_a(Numeric)
-      expect(image[:json][:type]).to be_a(String)
-      expect(image[:json][:webformatURL]).to be_a(String)
-      expect(image[:json][:user]).to be_a(String)
-      expect(image[:json][:tags]).to include('denver')
+      expect(data).to be_an(Hash)
+      expect(data[:images]).to be_a(Array)
+      expect(data[:images].count).to eq(10)
+      expect(data[:location]).to be_a(String)
+      expect(data[:location]).to eq(location)
 
-      expect(image[:other]).to be_a(Hash)
-      expect(image[:other][:location]).to be_a(String)
-      expect(image[:other][:source]).to be_a(String)
-      expect(image[:other][:logo]).to be_a(String)
+      data[:images].each do |link|
+        expect(link).to be_a(String)
+        expect(link).to include('jpg')
+      end
     end
   end
 end
