25daa3fcbd08392dd97c2e0b52982d48014177ef
Nico Rithner <nicorithner@gmail.com>
2020-12-20 18:10:01 -0700

    Refactor serialize_user method to use activerecord

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,24 +7,16 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def save_user(user, user_params)
-    serialize_user(user) unless screen_fields(user_params)
+    serialize_user(user)
   end
 
   def serialize_user(user)
-    user.save
-    user.api_key
-    render json: UsersSerializer.new(user), status: :created
-  end
-  
-  def screen_fields(user_params)
-    if User.find_by(email: user_params[:email])
-      render json: { message: 'Email already exists' }, status: :not_acceptable
-    elsif user_params[:email].nil?
-      render json: { message: 'Email cannot be blank' }, status: :not_acceptable
-    elsif user_params[:password].nil?
-      render json: { message: 'Password cannot be blank' }, status: :not_acceptable
-    elsif user_params[:password_confirmation] != user_params[:password]
-      render json: { message: "Passwords don't match" }, status: :not_acceptable
+    if user.valid?
+      user.save
+      user.api_key
+      render json: UsersSerializer.new(user), status: :created
+    else
+      render json: {errors: user.errors.full_messages},  status: :not_acceptable
     end
   end
 
